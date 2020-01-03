759bb906c2b9efdfcbeb822af70ee87edbfd088c
ramsey <ramsey.barghouti@gmail.com>
2020-01-03 15:23:06 -0500

    refactored urls a little

--- django_audio_manager/django_audio_manager/urls.py
+++ django_audio_manager/django_audio_manager/urls.py
@@ -1,31 +1,9 @@
-"""django_audio_manager URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
 import api.views as api_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "api/token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
-    ),
-    path(
-        "api/token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"
-    ),
-    path("api/ping", api_views.PingView.as_view(), name="api_ping"),
-    path("api/healthcheck", api_views.HealthCheck.as_view(), name="api_healthcheck"),
+    path("api/", include("api.urls")),
 ]
