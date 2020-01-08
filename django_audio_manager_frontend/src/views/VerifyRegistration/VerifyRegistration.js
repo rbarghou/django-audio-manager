84f2b796f851e27ae5662b9b2eb3ebc86fbf4168
ramsey <ramsey.barghouti@gmail.com>
2020-01-08 01:25:04 -0500

    implemented simple two step registration workflow

---
+++ django_audio_manager_frontend/src/views/VerifyRegistration/VerifyRegistration.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import { Link as RouterLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { verifyRegistration } from '../../actions/auth';
+import Cookies from 'js-cookie';
+import queryString from 'query-string';
+
+const VerifyRegistration = props => {
+  const { history, verifyRegistration, isAuthenticated } = props;
+  const { user_id, timestamp, signature } = queryString.parse(
+    props.location.search
+  );
+  verifyRegistration(
+    user_id,
+    timestamp,
+    signature,
+    Cookies.get('csrftoken'),
+    history
+  );
+
+  return <Fragment></Fragment>;
+};
+
+VerifyRegistration.propTypes = {
+  history: PropTypes.object,
+  verifyRegistration: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default withRouter(
+  connect(mapStateToProps, { verifyRegistration })(VerifyRegistration)
+);
