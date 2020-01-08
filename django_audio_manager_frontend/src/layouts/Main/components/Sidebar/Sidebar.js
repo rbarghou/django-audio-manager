420de7aa41f8a2888356509392f70d716e0d8472
ramsey <ramsey.barghouti@gmail.com>
2020-01-08 15:38:32 -0500

    profile is conditional on being authenticated.

--- django_audio_manager_frontend/src/layouts/Main/components/Sidebar/Sidebar.js
+++ django_audio_manager_frontend/src/layouts/Main/components/Sidebar/Sidebar.js
@@ -127,7 +127,7 @@ const Sidebar = props => {
       open={open}
       variant={variant}>
       <div {...rest} className={clsx(classes.root, className)}>
-        <Profile />
+        {isAuthenticated && <Profile />}
         <Divider className={classes.divider} />
         <SidebarNav className={classes.nav} pages={pages} />
       </div>
