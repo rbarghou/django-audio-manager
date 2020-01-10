e1f36849fb75f733e679a654dcf1a7dc0610e474
ramsey <ramsey.barghouti@gmail.com>
2020-01-09 23:29:53 -0500

    autopep8

--- django_audio_manager/manage.py
+++ django_audio_manager/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_audio_manager.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "django_audio_manager.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
