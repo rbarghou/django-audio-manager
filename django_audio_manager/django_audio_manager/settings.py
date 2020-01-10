9e486314860d6daeb8c61fab004c4afa2f3fb8e4
ramsey <ramsey.barghouti@gmail.com>
2020-01-10 18:13:37 -0500

    added uwsgi configuration

--- django_audio_manager/django_audio_manager/settings.py
+++ django_audio_manager/django_audio_manager/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/api/static/"
+STATIC_ROOT = "/app/static/"
 
 MEDIA_URL = "/api/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
